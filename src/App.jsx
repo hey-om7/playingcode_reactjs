9bb46cd54be627c6cc54c2b75fc2c2c66a0f3544
Om Ambarkar <heyom7@gmail.com>
2022-04-02 21:51:06 +0530

    firstCommit-testing on GHpages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        <img src="../logo.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
